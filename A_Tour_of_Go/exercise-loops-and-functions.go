ddb7aac0be3a47c5975b8a62d67f359214d88c1e
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-08-02 02:03:15 +0900

    add tour exercise

--- A_Tour_of_Go/exercise-loops-and-functions.go
+++ A_Tour_of_Go/exercise-loops-and-functions.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
-	return x * 2
+	z := 1.0
+	tmp := 1.0
+	for {
+		tmp -= (z*z - x) / (2 * z)
+		if math.Abs(z-tmp) > 1e-6 {
+			fmt.Println(z)
+			z = tmp
+		} else {
+			break
+		}
+	}
+	return z
 }
 
 func main() {
