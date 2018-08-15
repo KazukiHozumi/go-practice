ed6de47fc3d64497feba6a3971919313eb16dfbe
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-08-16 02:16:08 +0900

    add tour exercise

--- A_Tour_of_Go_exercise/example.go
+++ A_Tour_of_Go_exercise/example.go
@@ -1,24 +1,20 @@
 package main
 
-import "fmt"
-
-func adder() func(int) (int, int) {
-	sum := 0
-	count := 0
-	return func(n int) (int, int) {
-		sum += n
-		count++
-		return count, sum
-	}
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	adder1, adder2 := adder(), adder()
-	adder1(5)
-	adder1(5)
-	c1, s1 := adder1(5)
-	adder2(5)
-	c2, s2 := adder2(5)
+	ch := make(chan int)
+	go func() {
+		ch <- 1
+		ch <- 3
+		ch <- 2
+	}()
 
-	fmt.Println("c1: ", c1, "s1: ", s1, "c2: ", c2, "s2:", s2)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	time.Sleep(5 * time.Second)
+	fmt.Println(<-ch)
 }
--- A_Tour_of_Go_exercise/exercise-rot-reader.go
+++ A_Tour_of_Go_exercise/exercise-rot-reader.go
@@ -10,6 +10,19 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+func (r rot13Reader) Read(p []byte) (int, error) {
+	n, err := r.r.Read(p)
+	for i := 0; i < n; i++ {
+		if (p[i] >= 'a' && p[i] <= 'm') || (p[i] >= 'A' && p[i] <= 'M') {
+			p[i] = p[i] + 13
+		} else if (p[i] >= 'n' && p[i] <= 'z') || (p[i] >= 'n' && p[i] <= 'Z') {
+			p[i] = p[i] - 13
+		}
+	}
+
+	return n, err
+}
+
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
